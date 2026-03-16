d8194ee0fbe30f69401d68a0680cb60d66b92fbf
ocjdagreat1 <oparachibuikejustine@gmail.com>
2026-03-16 13:42:00 -0800

    new changes

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,5 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config.js/inngest";
+import { inngest,syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config.js/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
