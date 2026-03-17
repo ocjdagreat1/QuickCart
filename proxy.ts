73913beadb7e117a73eb748cdafd8f4f3638507e
ocjdagreat1 <oparachibuikejustine@gmail.com>
2026-03-17 15:26:42 -0800

    change middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export const middleware = clerkMiddleware();
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
